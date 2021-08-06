4da2561ee36e07728ecc1e48d163094cc23e134a
RDaneelOlivaw <duckfrost@gmail.com>
2021-08-06 19:06:48 +0200

    Added XACROI support

--- box_car_gazebo/launch/start_world_launch.py
+++ box_car_gazebo/launch/start_world_launch.py
@@ -15,7 +15,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
-    pkg_box_bot_gazebo = get_package_share_directory('box_bot_gazebo')
+    pkg_box_car_gazebo = get_package_share_directory('box_car_gazebo')
 
     # Gazebo launch
     gazebo = IncludeLaunchDescription(
@@ -27,7 +27,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
           'world',
-          default_value=[os.path.join(pkg_box_bot_gazebo, 'worlds', 'box_bot_empty.world'), ''],
+          default_value=[os.path.join(pkg_box_car_gazebo, 'worlds', 'box_bot_empty.world'), ''],
           description='SDF world file'),
         gazebo
     ])
